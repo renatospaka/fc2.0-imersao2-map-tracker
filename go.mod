9587bd90b57d7f17010952d44a8e9a06188e2c98
Renato Spakauskas <renatospaka@gmail.com>
2021-04-06 18:11:40 -0300

    feat: :sparkles: kafka consumer channel

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/renatospaka/fc2.0-imersao3-map-tracker
 
 go 1.16
+
+require github.com/confluentinc/confluent-kafka-go v1.6.1 // indirect
---
+++ infra/kafka/consumer.go
@@ -0,0 +1,38 @@
+package kafka
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type KafkaConsumer struct {
+	MsgChan chan *ckafka.Message
+}
+
+func (k *KafkaConsumer) Consume() {
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("KafkaBootstrapServers"),
+		"group.id":          os.Getenv("KafkaConsumerGroupId"),
+	}
+	c, err := ckafka.NewConsumer(configMap)
+	if err != nil {
+		log.Fatalf("Error consuming kafka messages: " + err.Error())
+	}
+
+	topics := []string{os.Getenv("KafkaReadTopics")}
+	c.SubscribeTopics(topics, nil)
+	fmt.Println("Kafka consumer has been started.")
+
+	//start consuming endlessly
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			//links the message to the channel
+			//it allows retrieving the messaging elsewhere in the app
+			k.MsgChan <- msg
+		}
+	}
+}
